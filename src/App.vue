e1280e9316c2efa24f410346280260fa9b893f3f
pengshiyu <1940607002@qq.com>
2023-03-31 17:42:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+// created at 2023-03-31
+export default {
+  name: "App",
+
+  props: {},
+
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  methods: {
+    async getData() {},
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
+
+<style lang="less"></style>
+
+<style lang="less" scoped></style>
